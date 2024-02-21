0728a3f28ee7e5edbf3a4c8f1956061f86a1349f
nachovy <zidaneandmessi@gmail.com>
2024-02-21 02:46:49 +0000

    debug False

--- mfpoj/settings.py
+++ mfpoj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n&ey+x!4phyv*b4cyv^i8fst2koi4!1f5foafdj&(!93@+t_z!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
